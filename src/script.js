c19a7435d009444cde3bf53bd584afc4fb385d1d
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2024-01-22 15:28:36 -0800

    Corrected formatting of humidity and wind functionality and added js time function using api

--- src/script.js
+++ src/script.js
@@ -3,10 +3,43 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityHeading = document.querySelector("#city-heading");
   let descriptionElement = document.querySelector("#description");
-  let description = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let wind = response.data.wind.speed;
+  let humidity = response.data.temperature.humidity;
+
   cityHeading.innerHTML = response.data.city;
-  descriptionElement.innerHTML = description
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${Math.round(wind)} mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours > 12) {
+    hours = `${hours - 12}`;
+    minutes = `${minutes} pm`;
+  } else {
+    minutes = `${minutes} am`;
+  }
+  return `${day} ${hours}:${minutes}, `;
 }
 
 function searchCity(city) {
