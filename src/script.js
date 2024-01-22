21978fe82c0a4472662e785195cd959811835229
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2024-01-22 14:33:53 -0800

    added js functionality to replace the current conditions

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   let cityHeading = document.querySelector("#city-heading");
+  let descriptionElement = document.querySelector("#description");
+  let description = response.data.condition.description;
   cityHeading.innerHTML = response.data.city;
+  descriptionElement.innerHTML = description
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
 function searchCity(city) {
   let apiKey = "0afcd2ao9bb24495ta6dd7a01113764b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
